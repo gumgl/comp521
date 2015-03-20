65f07aeb3ad162057216ec991ea8854fd1f1793c
Guillaume <gumgl@users.noreply.github.com>
2015-03-20 19:45:49 -0400

    /a3/Assets: Create Game class

---
+++ a3/Assets/Scripts/Game.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using System.Collections;
+
+public class Game : MonoBehaviour
+{
+	public Lane[] lanes = new Lane[3];
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+}
+

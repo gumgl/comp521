a8dcc020112a39656cff3179fbadbaf71e513a4b
Guillaume <gumgl@users.noreply.github.com>
2015-04-13 17:05:35 -0400

    /project/Assets/Scripts: Create Car class

---
+++ project/Assets/Scripts/Car.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class Car : MonoBehaviour
+{
+	private const int MIN_VECTORS = 8;
+	private const int MAX_VECTORS = 8;
+	public GameObject body;
+	private Mesh bodyMesh;
+
+	private List<VectorP> corners;
+
+	void init() {
+		
+	}
+
+	void initRandom() {
+		int numVectors = (MAX_VECTORS == MIN_VECTORS) ? MIN_VECTORS : Random.Range(MIN_VECTORS, MAX_VECTORS + 1);
+
+		for (int i = 0; i < numVectors; i++) {
+			VectorP newP = new VectorP();
+		}
+	}
+	
+	void Start ()
+	{
+	
+	}
+	void Update ()
+	{
+	
+	}
+}
